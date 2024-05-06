164af2d00de24be401e18644e783214ca93235e7
bhuiytas <165081550+bhuiytas@users.noreply.github.com>
2024-05-06 21:52:35 +0000

    party code

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-// Initial party checklist
-const partyChecklist = ["Music Playlist", "Snacks", "Decorations", "Games", "Invitations"];
 
-console.log("Initial Party Checklist:", partyChecklist);
+// Initial party checklist
+let partyChecklist = ["Music Playlist", "Snacks", "Decorations", "Games", "Invitations"];
+
+console.log("Initial Party Checklist:", ... partyChecklist);
 
 // Add 3 new items to the partyChecklist using a loop
-
+let party = ["Gift" , "Cake" , "Candles"]
+for (let i = 0; i < 3; i++){
+  partyChecklist.push(party[i]);
+}
+console.log("push: " + partyChecklist);
 // Remove 2 items from the partyChecklist using a loop
-
+for (let j = 0; j < 2; j++) {
+  partyChecklist.pop();
+}
+console.log("pop: " + partyChecklist)
 // Sort the partyChecklist in alphabetical order using a loop
+/* 
+for (let i =0; i < partyChecklist.length; i++) {
+  partyChecklist[i] = partyChecklist[i].toLocaleLowerCase();
+}
+partyChecklist.sort();
+console.log([...partyChecklist]); 
+^  cuz i forgot to capitalize the new variables before */
 
-console.log("Final Party Checklist:", partyChecklist);
+console.log("sort: " + partyChecklist.sort())
